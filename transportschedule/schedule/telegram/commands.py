5e05242b3f06f7c4cccbf296a37a0deadd9f380f
TurtleOld <dev@pavlovteam.ru>
2024-01-29 22:45:02 +0300

    added new route bus

--- transportschedule/schedule/telegram/commands.py
+++ transportschedule/schedule/telegram/commands.py
@@ -43,6 +43,24 @@ async def callback_handler_bus(call):
     await selected_route(call.message, process_result[:5])
 
 
+@bot.callback_query_handler(func=lambda call: call.data == 'north_bus_station')
+async def callback_handler_bus(call):
+    await bot.delete_message(call.message.chat.id, call.message.id)
+    await bot.send_message(
+        call.message.chat.id,
+        'Выбран маршрут: От Северного посёлка до Автовокзала Сергиев Посад',
+    )
+    request_data = RequestSchedule(
+        transport_types='bus',
+        from_station=9742891,
+        to_station=9742908,
+    )
+    json_data = request_data.request_transport_between_stations()
+    process = Processing(json_data)
+    process_result = process.detail_transport()
+    await selected_route(call.message, process_result[:5])
+
+
 async def start_bot():
     """Function for start telegram bot"""
     return await bot.infinity_polling()
