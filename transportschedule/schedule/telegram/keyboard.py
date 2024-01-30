bb5b85718905a2f9bcac1dfc6f11995008b3196d
TurtleOld <dev@pavlovteam.ru>
2024-01-30 16:50:05 +0300

    добавил клавиатуру для электричек + мелкие правки

--- transportschedule/schedule/telegram/keyboard.py
+++ transportschedule/schedule/telegram/keyboard.py
@@ -6,12 +6,18 @@ from transportschedule.schedule.telegram.config import bot
 
 async def select_transport_type(message):
     keyboard = InlineKeyboardMarkup(row_width=1)
-    bus = InlineKeyboardButton('Автобус', callback_data='bus')
-    suburban = InlineKeyboardButton('Электричка', callback_data='suburban')
+    bus = InlineKeyboardButton(
+        '\u00A0\u00A0Автобус\u00A0\u00A0',
+        callback_data='bus',
+    )
+    suburban = InlineKeyboardButton(
+        '\u00A0\u00A0Электричка\u00A0\u00A0',
+        callback_data='suburban',
+    )
     keyboard.add(bus, suburban)
     await bot.send_message(
         message.chat.id,
-        'Выбери тип транспорта',
+        '\u00A0\u00A0Выбери тип транспорта\u00A0\u00A0',
         reply_markup=keyboard,
     )
 
@@ -19,27 +25,95 @@ async def select_transport_type(message):
 async def selected_bus(message):
     keyboard = InlineKeyboardMarkup(row_width=1)
     bus_station_north = InlineKeyboardButton(
-        'Автовокзал-Северный',
+        '\u00A0\u00A0Автовокзал - Северный\u00A0\u00A0',
         callback_data='bus_station_north',
     )
     north_bus_station = InlineKeyboardButton(
-        'Северный-Автовокзал',
+        '\u00A0\u00A0Северный - Автовокзал\u00A0\u00A0',
         callback_data='north_bus_station',
     )
     keyboard.add(bus_station_north, north_bus_station)
     await bot.send_message(
         message.chat.id,
-        'Выбери направление',
+        '\u00A0\u00A0Выбери направление\u00A0\u00A0',
         reply_markup=keyboard,
     )
 
 
+async def selected_suburban(message):
+    keyboard = InlineKeyboardMarkup()
+    yaroslavsky_railway_station_sergiev_posad = InlineKeyboardButton(
+        '\u00A0\u00A0Ярославкий вокзал - Сергиев Посад\u00A0\u00A0',
+        callback_data='yaroslavsky_railway_station_sergiev_posad',
+    )
+    sergiev_posad_yaroslavsky_railway_station = InlineKeyboardButton(
+        '\u00A0\u00A0Сергиев Посад - Ярославкий вокзал\u00A0\u00A0',
+        callback_data='sergiev_posad_yaroslavsky_railway_station',
+    )
+
+    yaroslavsky_railway_station_podlipki = InlineKeyboardButton(
+        '\u00A0\u00A0Ярославкий вокзал - Подлипки-Дачные\u00A0\u00A0',
+        callback_data='yaroslavsky_railway_station_podlipki',
+    )
+    podlipki_yaroslavsky_railway_station = InlineKeyboardButton(
+        '\u00A0\u00A0Подлипки-Дачные - Ярославкий вокзал\u00A0\u00A0',
+        callback_data='podlipki_yaroslavsky_railway_station',
+    )
+
+    podlipki_mytischi = InlineKeyboardButton(
+        '\u00A0\u00A0Подлипки-Дачные - Мытищи\u00A0\u00A0',
+        callback_data='podlipki_mytischi',
+    )
+    mytischi_podlipki = InlineKeyboardButton(
+        '\u00A0\u00A0Мытищи - Подлипки-Дачные\u00A0\u00A0',
+        callback_data='mytischi_podlipki',
+    )
+    yaroslavsky_railway_station_mytischi = InlineKeyboardButton(
+        '\u00A0\u00A0Ярославкий вокзал - Мытищи\u00A0\u00A0',
+        callback_data='yaroslavsky_railway_station_mytischi',
+    )
+
+    sergiev_posad_mytischi = InlineKeyboardButton(
+        '\u00A0\u00A0Сергиев Посад - Мытищи\u00A0\u00A0',
+        callback_data='sergiev_posad_mytischi',
+    )
+
+    black_serp_molot = InlineKeyboardButton(
+        '\u00A0\u00A0Чёрное - Серп и Молот\u00A0\u00A0',
+        callback_data='black_serp_molot',
+    )
+    serp_molot_black = InlineKeyboardButton(
+        '\u00A0\u00A0Серп и Молот - Чёрное\u00A0\u00A0',
+        callback_data='serp_molot_black',
+    )
+    keyboard.row(
+        yaroslavsky_railway_station_sergiev_posad,
+        sergiev_posad_yaroslavsky_railway_station,
+    )
+    keyboard.add(
+        yaroslavsky_railway_station_podlipki,
+        podlipki_yaroslavsky_railway_station,
+        podlipki_mytischi,
+        mytischi_podlipki,
+        yaroslavsky_railway_station_mytischi,
+        sergiev_posad_mytischi,
+        black_serp_molot,
+        serp_molot_black,
+    )
+    await bot.send_message(
+        message.chat.id,
+        'Выбери направление',
+        reply_markup=keyboard,
+        parse_mode='HTML',
+    )
+
+
 async def selected_route(message, route_info):
-    keyboard = InlineKeyboardMarkup(row_width=3)
+    keyboard = InlineKeyboardMarkup()
     for route in route_info:
-        keyboard.add(
+        keyboard.row(
             InlineKeyboardButton(
-                text=route,
+                text=f'\u00A0\u00A0\u00A0{route}\u00A0\u00A0\u00A0',
                 callback_data=' '.join(route.split()[:5]),
             )
         )
