729499e430aa585ae3dd857b3e772ba7f11c5b31
TurtleOld <dev@pavlovteam.ru>
2024-01-30 16:50:42 +0300

    выделил обработку запросов в отдельную функцию.

--- transportschedule/schedule/telegram/commands.py
+++ transportschedule/schedule/telegram/commands.py
@@ -7,6 +7,7 @@ from transportschedule.schedule.telegram.keyboard import (
     select_transport_type,
     selected_bus,
     selected_route,
+    selected_suburban,
 )
 
 
@@ -25,40 +26,79 @@ async def callback_handler_bus(call):
     await selected_bus(call.message)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'bus_station_north')
-async def callback_handler_bus(call):
+@bot.callback_query_handler(func=lambda call: call.data == 'suburban')
+async def callback_handler_suburban(call):
     await bot.delete_message(call.message.chat.id, call.message.id)
     await bot.send_message(
         call.message.chat.id,
-        'Выбран маршрут: От Автовокзала Сергиев Посад до Северного посёлка',
+        'Выбран вид транспорта: Электричка',
     )
-    request_data = RequestSchedule(
-        transport_types='bus',
-        from_station=9742908,
-        to_station=9742891,
-    )
-    json_data = request_data.request_transport_between_stations()
+    await selected_suburban(call.message)
+
+
+async def handler_request_transport(call):
+    if call.data == 'bus_station_north':
+        await bot.send_message(
+            call.message.chat.id,
+            'Выбран маршрут: От Автовокзала Сергиев Посад до Северного посёлка',
+        )
+        request_data = RequestSchedule(
+            transport_types='bus',
+            from_station=9742908,
+            to_station=9742891,
+        )
+        return request_data.request_transport_between_stations()
+    elif call.data == 'north_bus_station':
+        await bot.send_message(
+            call.message.chat.id,
+            'Выбран маршрут: От Северного посёлка до Автовокзала Сергиев Посад',
+        )
+        request_data = RequestSchedule(
+            transport_types='bus',
+            from_station=9742891,
+            to_station=9742908,
+        )
+        return request_data.request_transport_between_stations()
+    elif call.data == 'yaroslavsky_railway_station_sergiev_posad':
+        await bot.send_message(
+            call.message.chat.id,
+            'Выбран маршрут: От Ярославского вокзала до Сергиев Посада',
+        )
+        request_data = RequestSchedule(
+            transport_types='suburban',
+            from_station=2000002,
+            to_station=9601389,
+        )
+        return request_data.request_transport_between_stations()
+
+
+@bot.callback_query_handler(func=lambda call: True)
+async def callback_handler_bus_route(call):
+    await bot.delete_message(call.message.chat.id, call.message.id)
+    json_data = await handler_request_transport(call)
     process = Processing(json_data)
     process_result = process.detail_transport()
     await selected_route(call.message, process_result[:5])
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'north_bus_station')
-async def callback_handler_bus(call):
-    await bot.delete_message(call.message.chat.id, call.message.id)
-    await bot.send_message(
-        call.message.chat.id,
-        'Выбран маршрут: От Северного посёлка до Автовокзала Сергиев Посад',
-    )
-    request_data = RequestSchedule(
-        transport_types='bus',
-        from_station=9742891,
-        to_station=9742908,
-    )
-    json_data = request_data.request_transport_between_stations()
-    process = Processing(json_data)
-    process_result = process.detail_transport()
-    await selected_route(call.message, process_result[:5])
+# @bot.callback_query_handler(
+#     func=lambda call: call.data == 'yaroslavsky_railway_station_sergiev_posad'
+# )
+# async def callback_handler_suburban(call):
+#     await bot.delete_message(call.message.chat.id, call.message.id)
+#     await bot.send_message(
+#         call.message.chat.id,
+#         'Выбран маршрут: От Ярославского вокзала до Сергиев Посада',
+#     )
+#     request_data = RequestSchedule(
+#         transport_types='suburban',
+#         from_station=2000002,
+#         to_station=9601389,
+#     )
+#     json_data = request_data.request_transport_between_stations()
+#     process = Processing(json_data)
+#     process_result = process.detail_transport()
+#     await selected_route(call.message, process_result[:5])
 
 
 async def start_bot():
