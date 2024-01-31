70e79e0b0ee938e044fb2d07caa69f36988bf618
TurtleOld <dev@pavlovteam.ru>
2024-01-31 19:42:56 +0300

    Добавил webhooks

--- transportschedule/engine.py
+++ transportschedule/engine.py
@@ -1,14 +1,7 @@
 import asyncio
-import datetime
-import json
 import os
-import ssl
 
 from aiohttp import web
-from icecream import ic
-
-from transportschedule.schedule.process.processing import Processing
-from transportschedule.schedule.request.request import RequestSchedule
 from transportschedule.schedule.telegram.commands import start_bot, setup
 from transportschedule.schedule.telegram.config import WEBHOOK_LISTEN, WEBHOOK_PORT
 
@@ -18,12 +11,10 @@ def main():
     if os.getenv('DEBUG'):
         asyncio.run(start_bot())
     else:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         web.run_app(
             setup(),
             host=WEBHOOK_LISTEN,
             port=WEBHOOK_PORT,
-            ssl_context=context,
         )
 
 
