c1f03a2d2a0277017f13616b585ad42d8474765d
TurtleOld <dev@pavlovteam.ru>
2024-02-01 06:08:54 +0300

    добавил информация об остановках на маршруте

--- transportschedule/schedule/process/processing.py
+++ transportschedule/schedule/process/processing.py
@@ -15,9 +15,10 @@ def convert_time(seconds: float):
 
 
 class Processing:
-    def __init__(self, json_data):
+    def __init__(self, json_data, route_stops=None):
         self.json_data = json_data
         self.parser = JsonParser(self.json_data)
+        self.route_stops = set(route_stops)
 
     def pagination(self):
         limit = self.parser.parse_json(self.json_data, 'limit')
@@ -40,10 +41,11 @@ class Processing:
             title_to,
         )
 
-    def detail_transport(self) -> tuple[list, list]:
+    def detail_transport(self) -> tuple[list, list, list]:
         segments = self.parser.parse_json(self.json_data, 'segments')
         route_info: list = list()
         route_detail_info: list = list()
+        route_stops: list = list()
         utc_offset = timedelta(hours=3)
         current_time = timezone(utc_offset)
         current_datetime = datetime.now(current_time)
@@ -66,6 +68,7 @@ class Processing:
                     'short_title',
                 )
                 uid_thread = self.parser.parse_json(thread_route, 'uid')
+                stops = self.parser.parse_json(segment, 'stops')
                 route_info.append(
                     '\u00A0\u00A0#{1} | {0} ({3}) | {2}\u00A0\u00A0'.format(
                         departure_format_date,
@@ -77,15 +80,37 @@ class Processing:
                 route_detail_info.append(
                     uid_thread,
                 )
-        return route_info, route_detail_info
+                route_stops.append(stops)
+                if 'кроме' in route_stops:
+                    route_stops.remove('везде')
+        return route_info, route_detail_info, route_stops
 
     def detail_thread(self):
-        ic(self.json_data)
         number = self.parser.parse_json(self.json_data, 'number')
         short_title = self.parser.parse_json(self.json_data, 'short_title')
         days = self.parser.parse_json(self.json_data, 'days')
         from_station = self.parser.parse_json(self.json_data, 'from')
-        to_station = self.parser.parse_json(self.json_data, 'to')
-        return (
-            '<strong>Электричка:</strong> {} {}\n <strong>График хождения:</strong> {}'
-        ).format(number, short_title, days)
+        transport_type = self.parser.parse_json(
+            from_station,
+            'transport_type',
+        )
+
+        route_stops = ' '.join(self.route_stops)
+        if not self.route_stops:
+            route_stops += 'везде'
+
+        from_title = self.parser.parse_json(from_station, 'title')
+        stops = self.parser.parse_json(self.json_data, 'stops')
+        stop_departure = ''
+        for stop in stops:
+            stop_station = self.parser.parse_json(stop, 'station')
+            stop_station_title = self.parser.parse_json(stop_station, 'title')
+            if from_title == stop_station_title:
+                stop_departure += stop['departure'][10:]
+        transport_type_name = 'Электричка'
+        if transport_type == 'bus':
+            transport_type_name = 'Автобус'
+        return f'''<strong>{transport_type_name}:</strong> {number} {short_title}
+<strong>График хождения:</strong> {days}
+<strong>Время отправления:</strong> {stop_departure}
+<strong>С остановками:</strong> {route_stops}'''
