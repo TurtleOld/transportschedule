326a1c49ea81b5f3e98dce6d887d293f8f07892d
TurtleOld <dev@pavlovteam.ru>
2024-02-07 13:34:00 +0300

    added tests

---
+++ transportschedule/schedule/tests/test_api_yandex.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+
+import requests
+from dotenv import load_dotenv
+from icecream import ic
+
+from transportschedule import constants
+from transportschedule.schedule.request.request import RequestSchedule
+
+load_dotenv()
+
+
+def test_request_transport_between_stations():
+    request = RequestSchedule(
+        transport_types='bus',
+        from_station=constants.BUS_STATION_SERGIEV_POSAD,
+        to_station=constants.BUS_STOP_NORTH_VILLAGE,
+        date=datetime.datetime.now().isoformat(),
+    )
+    response = request.request_transport_between_stations()
+
+    assert response.status_code == 200
+
+
+def test_api_error():
+
+    params = {
+        'apikey': os.getenv('YANDEX_API_KEY'),
+        'transport_types': 'bus',
+        'from': 97429,
+        'to': 97428,
+        'date': datetime.datetime.now().isoformat(),
+        'limit': 500,
+    }
+
+    url_search = 'https://api.rasp.yandex.net/v3.0/search/'
+
+    response = requests.get(url_search, params=params)
+
+    assert response.status_code == 404
+
+    assert (
+        response.json().get('error').get('text')
+        == 'Не нашли объект по yandex коду 97429'
+    )
--- transportschedule/schedule/tests/test_json_parse.py
+++ transportschedule/schedule/tests/test_json_parse.py
@@ -1,12 +1,30 @@
+import json
 import pathlib
 
 from icecream import ic
+
+from transportschedule.schedule.json_parse.json_parser import JsonParser
 
 
 def test_json_parse():
     file = pathlib.Path(
         'transportschedule/schedule/tests/test_data/request.json',
     )
+
     with open(file) as json_data:
-        data = json_data.read()
-        ic(data)
+        data = json.load(json_data)
+
+        parser = JsonParser(data)
+
+        assert parser.parse_json(data, 'to') == {
+            "code": "s9742891",
+            "popular_title": "none",
+            "short_title": "none",
+            "station_type": "bus_stop",
+            "station_type_name": "автобусная остановка",
+            "title": "Северный посёлок",
+            "transport_type": "bus",
+            "type": "station",
+        }
+
+        assert parser.parse_json(data, 'to', 'title') == 'Северный посёлок'
