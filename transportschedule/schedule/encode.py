88093dcba2867d994541832291287b0749dfb489
TurtleOld <dev@pavlovteam.ru>
2024-02-14 22:19:49 +0300

    fix

--- transportschedule/schedule/encode.py
+++ transportschedule/schedule/encode.py
@@ -1,8 +1,6 @@
 import hashlib
 import os
 
-from icecream import ic
-
 
 def encode_string(string):
     salt = os.urandom(16)
@@ -12,7 +10,7 @@ def encode_string(string):
     return salt, login
 
 
-def check_login(salt, string):
-    bstring = string.encode('utf-8')
+def check_login(salt, username):
+    bstring = username.encode('utf-8')
     key = hashlib.pbkdf2_hmac('sha256', bstring, salt, 10000)
     return key
