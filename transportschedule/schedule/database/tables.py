e2ab5636715a5ea808bf9f13521fd081adccd120
TurtleOld <dev@pavlovteam.ru>
2024-02-15 22:30:43 +0300

    refactoring

--- transportschedule/schedule/database/tables.py
+++ transportschedule/schedule/database/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, BLOB
 from sqlalchemy.orm import Mapped, mapped_column
 
 from transportschedule.schedule.database.connect import Base
@@ -8,9 +8,9 @@ class UserRoute(Base):
     __tablename__ = "user_route"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(40))
+    username: Mapped[str] = mapped_column(BLOB)
     thread: Mapped[str] = mapped_column(String(40))
     number: Mapped[str] = mapped_column(String(10))
     from_station: Mapped[str] = mapped_column(String(10))
     to_station: Mapped[str] = mapped_column(String(10))
-    salt: Mapped[str] = mapped_column(String(100))
+    salt: Mapped[str] = mapped_column(BLOB)
